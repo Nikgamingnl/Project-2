0c4f56b5defa26693c884a70aad369c9ae8bd54d
Nikgamingnl <nikwarmerdam@gmail.com>
2026-05-21 11:09:00 +0200

    change

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#define BMES 2
+#define ESPCLK 4
+#define DPSDA 16
+#define ROENS 17
+#define MQS 26
+#define BVS 27
+#define HEATING PAD 13
+#define GROW LIGHT 32
+#define HUMIDIFIER 12
+#define WATERPOMP 14
+#define INPUT1 23
+#define INPUT2 21
+#define INPUT3 19
+#define INPUT4 5
+#define ENABLEA 22
+#define ENABLEB 24
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup{
+ Serial.begin(115200);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+  display.clearDisplay();
 }
